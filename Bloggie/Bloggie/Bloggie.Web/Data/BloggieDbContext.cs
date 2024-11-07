2fa37517aa54759b24bc7975c51788a88a9df0dc
PJO-A <info@pjo-automation.co.uk>
2024-11-07 10:36:30 -0800

    Initial commit

---
+++ Bloggie/Bloggie/Bloggie.Web/Data/BloggieDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Bloggie.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+            
+
+        }
+
+        // properties to represent domain model
+        public DbSet<BlogPost> BlogPosts { get; set; }  //DbSet of type: BlogPost , BlogPosts - name from SQL
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
